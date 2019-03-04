51b2f8d4b9d4d1fea0ea4fa648335c7a28de050f
decentralizedarchive <45758444+therealcurlsport@users.noreply.github.com>
2019-03-04 15:43:19 -0700

    Create dotnet-framework.php

---
+++ dotnet-framework.php
@@ -0,0 +1,75 @@
+gotcha/ipdb
+Integration of IPython pdb
+python
+debugger
+ipython
+Updated on Jan 24
+
+ Python
+ 935
+devhawk/ipydbg
+managed debugger for IronPython
+Updated on Apr 8, 2009
+
+ C#
+ 22
+zhovner/Skype-iplookup
+Perform obscure ip lookup for online skype accounts. Can find local and remote ip address. Require craked SkypeKit wi…
+Updated on Apr 30, 2012
+
+ Python
+ 137
+huuck/ADBHoney
+Low interaction honeypot designed for Android Debug Bridge over TCP/IP
+GPL-3.0 license
+
+Updated on Dec 10, 2018
+
+ Python
+ 90
+tkf/ipython-doctestmagic
+Run and debug doctest in IPython
+Updated on Oct 12, 2018
+
+ Python
+ 5
+sektioneins/ipod-serial-adapter
+circuit board (PCB) schematics for 30-pin iPod serial debugging
+Updated on Aug 7, 2017
+
+ KiCad Layout
+ 38
+akaihola/ipdb
+Fork of the IPython-enabled pdb debugger, now with nose compatibility
+Updated on Aug 25, 2011
+
+ Python
+ 4
+gregoil/ipdbugger
+Debugging utility that helps you inspect your code
+MIT license
+
+Updated on Jan 29
+
+ Python
+ 15
+cocos-creator-packages/ipc-debugger
+Ipc Debugger for Editor Framework
+MIT license
+
+Updated on May 13, 2016
+
+ JavaScript
+ 2
+PMoureu/iph
+IronPython Helper - Doc Reader - Debugger (WPF, WinForms, RevitAPI)
+wpf
+ironpython
+xaml
+autodesk-revit
+revitpythonshell
+pyrevit
+Updated on Apr 20, 2018
+
+ Python
+ 12
